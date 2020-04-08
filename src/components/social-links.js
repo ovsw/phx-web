ce5a0c78e6ff5436add0f11314fb4843db6977ee
Ovi Savescu <ovi@ovswebsites.com>
2020-04-08 10:16:48 +0300

    added google my business social link

--- src/components/social-links.js
+++ src/components/social-links.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {FaTwitterSquare, FaLinkedinIn, FaFacebookSquare, FaInstagram} from 'react-icons/fa'
+import {IoLogoGoogle} from 'react-icons/io'
 
 const SocialLinks = () => {
   return (
@@ -8,6 +9,7 @@ const SocialLinks = () => {
       <li><a href='https://www.linkedin.com/in/jimmy-vercellino-29060930/' rel='noopener noreferrer' target='_blank' title='LinkedIn'><FaLinkedinIn /></a></li>
       <li><a href='http://www.facebook.com/TheVercellinoTeam'rel='noopener noreferrer' target='_blank' title='Facebok'><FaFacebookSquare /></a></li>
       <li><a href='https://www.instagram.com/jimmyvercellino_/' rel='noopener noreferrer' target='_blank' title='Instagram'>{<FaInstagram />}</a></li>
+      <li><a href='https://www.google.com/maps/uv?hl=en&pb=!1s0x872b74ce3a1f36a7%3A0x64772747e819f813!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMm2fmD7HPPGlxUhUJt7W2TUyB3UgIoESnBJlaE%3Dw160-h160-k-no!5sphxhomeloan%20-%20Google%20Search!15sCAQ&imagekey=!1e10!2sAF1QipMw5F1B_cDEiMlXBd0OFYdGSq2ov7QQYB95Zmfv&sa=X&ved=2ahUKEwiunsWyl9joAhWmtYsKHTZcCqoQoiowE3oECB4QBg' rel='noopener noreferrer' target='_blank' title='Google My Business'>{<IoLogoGoogle />}</a></li>
     </>
   )
 }
