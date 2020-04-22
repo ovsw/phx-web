53e560d662673b25736825f4f40071944c103acf
Ovi Savescu <ovi@ovswebsites.com>
2020-04-22 16:20:37 +0300

    added youtube link

--- src/components/social-links.js
+++ src/components/social-links.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {FaTwitterSquare, FaLinkedinIn, FaFacebookSquare, FaInstagram} from 'react-icons/fa'
+import {FaTwitterSquare, FaLinkedinIn, FaFacebookSquare, FaInstagram, FaYoutubeSquare} from 'react-icons/fa'
 import {IoLogoGoogle} from 'react-icons/io'
 
 const SocialLinks = () => {
   return (
     <>
+      <li><a href='https://www.youtube.com/channel/UCmSqQXiy5BIxh-hZYp_RDWw'rel='noopener noreferrer' target='_blank' title='YouTube'><FaYoutubeSquare /></a></li>
       <li><a href='https://twitter.com/phxhomeloan'rel='noopener noreferrer' target='_blank' title='Twitter'><FaTwitterSquare /></a></li>
       <li><a href='https://www.linkedin.com/in/jimmy-vercellino-29060930/' rel='noopener noreferrer' target='_blank' title='LinkedIn'><FaLinkedinIn /></a></li>
       <li><a href='http://www.facebook.com/TheVercellinoTeam'rel='noopener noreferrer' target='_blank' title='Facebok'><FaFacebookSquare /></a></li>
